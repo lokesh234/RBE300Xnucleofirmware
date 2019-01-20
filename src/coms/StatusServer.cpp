576ca775b9fc6fa666ec2b40f2837b2120f79a57
Arjun Gandhi <arjungandhi06@gmail.com>
2019-01-20 15:07:40 -0500

    added status server C++ filed & updated main.cpp to reflect changes

--- src/coms/StatusServer.cpp
+++ src/coms/StatusServer.cpp
@@ -2,6 +2,30 @@
 
 void StatusServer::event(float *buffer) {
 
-  // DO something
-  // set values back to Buffer
-}
+  /*
+   * ======= PART 1: Generate a response to be sent back to MATLAB =============
+   */
+  void StatusServer::event(float *packet) {
+    // we will be using the same memory area in which the incoming packet was
+    // stored, however, a we need to perform a type cast first (for
+    // convenience).
+    uint8_t *buff = (uint8_t *)packet;
+
+    // re-initialize the packet to all zeros
+    for (int i = 0; i < 60; i++)
+      buff[i] = 0;
+
+    /**
+     * The following loop reads sensor data (encoders ticks, joint velocities
+     * and force readings) and writes it in the response packet.
+     */
+    for (int i = 0; i < myPumberOfPidChannels; i++) {
+      float position = myPidObjects[i]->GetPIDPosition();
+      float velocity = myPidObjects[i]->getVelocity();
+      // float torque = myPidObjects[i]->loadCell->read();
+
+      packet[(i * 3) + 0] = position;
+      packet[(i * 3) + 1] = velocity;
+      // packet[(i * 3) + 2] = torque;
+    }
+  }
--- src/coms/StatusServer.h
+++ src/coms/StatusServer.h
@@ -4,13 +4,24 @@
 #include <PID_Bowler.h>
 #include <PacketEvent.h>
 #include <cmath> // std::abs
-#define STATUSSERVERID 99
+#define STATUSSERVERID 03
 
 class StatusServer : public PacketEventAbstract {
 
 public:
   // Packet ID needs to be set
-  StatusServer() : PacketEventAbstract(STATUSSERVERID) {}
+  StatusServer() : PacketEventAbstract(STATUSSERVERID) {
+  private:
+    PIDimp **myPidObjects; // array of PidServers - one for each joint
+    int myPumberOfPidChannels;
+
+  public:
+    StatusServer(PIDimp * *pidObjects, int numberOfPidChannels)
+        : PacketEventAbstract(PID_SERVER_ID) {
+      myPidObjects = pidObjects;
+      myPumberOfPidChannels = numberOfPidChannels;
+    }
+  }
   // User function to be called when a packet comes in
   // Buffer contains data from the packet coming in at the start of the function
   // User data is written into the buffer to send it back
