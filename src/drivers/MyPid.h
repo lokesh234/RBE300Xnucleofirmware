3149564d578e9d4accd73626104fe8a639d8f4f6
Matt Schueler <mjschueler@gmail.com>
2019-01-20 21:48:00 -0500

    tuned PID gains

--- src/drivers/MyPid.h
+++ src/drivers/MyPid.h
@@ -22,9 +22,9 @@
 #include "RunEvery.h"
 
 // Initial values for the PID gains
-#define kp 0.005
-#define ki 0
-#define kd 0
+#define kp 0.00295
+#define ki 0.000249
+#define kd 0.000562
 #define vkp 1
 
 #define vkd 0
