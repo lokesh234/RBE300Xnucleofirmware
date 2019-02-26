5a171a0786c2f77f4425815fa307592f29f1962c
Matt Schueler <mjschueler@gmail.com>
2019-02-26 01:41:23 -0500

    fixed starting location and formatting

--- src/coms/GripperServer.cpp
+++ src/coms/GripperServer.cpp
@@ -3,10 +3,10 @@
 void GripperServer::event(float * buffer){
 	Servo myservo(GRIPPER_SERVO, 5);
 	if(buffer[3] == 1){
-	myservo = 0.8;
-	wait(2);
+		myservo = 0.8;
+		wait(2);
 	}
 	if(buffer[3] == 0){
-	myservo = 0;
+		myservo = 0;
 	}
 }
