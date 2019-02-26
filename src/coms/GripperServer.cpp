08954add1b40e8ffdd35e058e70e97dee8c05f0a
lokesh234 <lgangaramaney@wpi.edu>
2019-02-26 13:57:20 -0500

    Make IT NOT MAKE NOISE

--- src/coms/GripperServer.cpp
+++ src/coms/GripperServer.cpp
@@ -2,11 +2,11 @@
 
 void GripperServer::event(float * buffer){
 	Servo myservo(GRIPPER_SERVO, 5);
-	if(buffer[3] == 1){
+	if(buffer[3] == 6){
 	myservo = 0.8;
 	wait(2);
 	}
-	if(buffer[3] == 0){
+	if(buffer[3] == 5){
 	myservo = 0;
 	}
 }
